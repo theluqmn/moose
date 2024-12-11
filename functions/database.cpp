e86b6c07378e8626cadcfcaf721f033ef6af620e
theluqmn <luqman.yazrie@outlook.com>
2024-12-11 15:43:39 +0800

    create transactions table

--- functions/database.cpp
+++ functions/database.cpp
@@ -23,3 +23,25 @@ sqlite3* initAccountsDB() {
 
     return db;
 }
+
+sqlite3* initTransactionsDB() {
+    // Open the database
+    sqlite3 *db;
+    int res = sqlite3_open("transactions.db", &db);
+    if (res != SQLITE_OK) {
+        std::cerr << "Error opening database: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return nullptr;
+    }
+
+    // Create the transactions table
+    std::string transactions_sql = "CREATE TABLE IF NOT EXISTS transactions (sender_id INTEGER, receiver_id INTEGER, amount FLOAT, description TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+    res = sqlite3_exec(db, transactions_sql.c_str(), NULL, NULL, NULL);
+    if (res != SQLITE_OK) {
+        std::cerr << "Error creating table: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return nullptr;
+    }
+
+    return db;
+}
