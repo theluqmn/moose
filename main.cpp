4f1cc3d1ab0c987373f3c85ad0372695e3f35328
theluqmn <luqman.yazrie@outlook.com>
2024-12-11 13:20:42 +0800

    bash script to handle compilations

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
 
+#include "functions/payment.h"
+
 int main() {
     std::cout << "Hello! says the machine" << std::endl;
 
     std::string name;
     std::cout << "Enter your name: ";
-    std::cin >> name;
+    std::getline(std::cin, name);
 
     std::cout << "Hello " << name << "!" << std::endl;
+
+    printPayment();
 
     return 0;
 }
