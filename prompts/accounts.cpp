8f5535c9362132d7738815694f82f35057f95507
theluqmn <luqman.yazrie@outlook.com>
2024-12-14 08:43:21 +0800

    started work on new open account prompt

--- prompts/accounts.cpp
+++ prompts/accounts.cpp
@@ -4,38 +4,45 @@
 
 using namespace std;
 
+// void promptOpenAccount() {
+//     string accountName;
+//     string accountPassword;
+//     int accountID;
+
+//     cout << "[ Create account ]" << endl;
+
+//     // Account owner's name
+//     cout << "(1/3) Account owner's name: ";
+//     getline(cin, accountName);
+
+//     // Password
+//     cout << "(2/3) Password: ";
+//     getline(cin, accountPassword);
+
+//     // Verify account information
+//     cout << "(3/3) Verify account information:" << endl;
+//     cout << "- Account owner's name: " << accountName << endl;
+//     cout << "- Password: " << accountPassword << endl;
+//     cout << endl;
+
+//     cout << "Is this information correct? (y/n): ";
+//     string confirmation;
+//     getline(cin, confirmation);
+
+//     // Execute account creation
+//     if (confirmation == "y") {
+//         accountID = openAccount(accountName, accountPassword);
+
+//         cout << "Account created successfully!" << endl;
+//         cout << "ID: " << accountID << endl;
+//     } else {
+//         cout << "Account creation process aborted" << endl;
+//     }
+// }
+
 void promptOpenAccount() {
     string accountName;
     string accountPassword;
+    string accountType;
     int accountID;
-
-    cout << "[ Create account ]" << endl;
-
-    // Account owner's name
-    cout << "(1/3) Account owner's name: ";
-    getline(cin, accountName);
-
-    // Password
-    cout << "(2/3) Password: ";
-    getline(cin, accountPassword);
-
-    // Verify account information
-    cout << "(3/3) Verify account information:" << endl;
-    cout << "- Account owner's name: " << accountName << endl;
-    cout << "- Password: " << accountPassword << endl;
-    cout << endl;
-
-    cout << "Is this information correct? (y/n): ";
-    string confirmation;
-    getline(cin, confirmation);
-
-    // Execute account creation
-    if (confirmation == "y") {
-        accountID = openAccount(accountName, accountPassword);
-
-        cout << "Account created successfully!" << endl;
-        cout << "ID: " << accountID << endl;
-    } else {
-        cout << "Account creation process aborted" << endl;
-    }
 }
