8dd7a09fcf44ddc8b5f0718910c00a53ca08c0a4
theluqmn <luqman.yazrie@outlook.com>
2024-12-17 03:27:04 +0800

    started work on prompt for closing accounts, also got rid of the first version for opening accounts (in comments)

--- prompts/accounts.cpp
+++ prompts/accounts.cpp
@@ -4,42 +4,6 @@
 
 using namespace std;
 
-// void promptOpenAccount() {
-//     string accountName;
-//     string accountPassword;
-//     int accountID;
-
-//     cout << "[ Create account ]" << endl;
-
-//     // Account owner's name
-//     cout << "(1/3) Account owner's name: ";
-//     getline(cin, accountName);
-
-//     // Password
-//     cout << "(2/3) Password: ";
-//     getline(cin, accountPassword);
-
-//     // Verify account information
-//     cout << "(3/3) Verify account information:" << endl;
-//     cout << "- Account owner's name: " << accountName << endl;
-//     cout << "- Password: " << accountPassword << endl;
-//     cout << endl;
-
-//     cout << "Is this information correct? (y/n): ";
-//     string confirmation;
-//     getline(cin, confirmation);
-
-//     // Execute account creation
-//     if (confirmation == "y") {
-//         accountID = openAccount(accountName, accountPassword);
-
-//         cout << "Account created successfully!" << endl;
-//         cout << "ID: " << accountID << endl;
-//     } else {
-//         cout << "Account creation process aborted" << endl;
-//     }
-// }
-
 void promptOpenAccount() {
     string accountName;
     string accountPassword;
@@ -49,6 +13,7 @@ void promptOpenAccount() {
     cout << "[ Create Account ]" << endl;
     cout << "Available account types:" << endl;
     cout << "- current      - Current account" << endl;
+    cout << endl;
 
     // Account type
     cout << "(1/4) Account type: ";
@@ -81,4 +46,29 @@ void promptOpenAccount() {
     } else {
         cout << "Aborted process" << endl;
     }
+}
+
+void promptCloseAccount() {
+    string accountPassword;
+    int accountID;
+
+    cout << "[ Close Account ]" << endl;
+    cout << "(1/3) Enter the account ID: ";
+    string accountIDInput;
+    getline(cin, accountIDInput);
+    accountID = stoi(accountIDInput);
+
+    cout << "(2/3) Enter the account password: ";
+    getline(cin, accountPassword);
+
+    // Verify account info
+    cout << "(3/3) Verify account information:" << endl;
+    cout << "- Account ID: " << accountID << endl;
+    cout << "- Password: " << accountPassword << endl;
+    cout << "WARNING: This action is irreversible" << endl;
+    cout << endl;
+
+    string confirmation;
+    cout << "Proceed with closing? (y/n): ";
+    getline(cin, confirmation);
 }
