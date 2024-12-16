4b124962a6806cb55a2484049705846b913641d7
theluqmn <luqman.yazrie@outlook.com>
2024-12-17 04:23:42 +0800

    encountering a bug with account closing

--- functions/accounts/close.cpp
+++ functions/accounts/close.cpp
@@ -11,24 +11,24 @@ int closeAccount(int accountID, string accountPassword) {
     int exists = accountExists(accountID);
 
     if (exists == 1) {
-        // Open the database
-        sqlite3 *db = initAccountsDB();
-        if (db == nullptr) {
-            return -1;
-        }
-
-        // Close the account
-        string sql = "DELETE FROM current WHERE account_id = " + to_string(accountID) + ";";
-        int res = sqlite3_exec(db, sql.c_str(), NULL, NULL, NULL);
-        if (res != SQLITE_OK) {
-            cerr << "Error closing account: " << sqlite3_errmsg(db) << endl;
+        sqlite3* db = initAccountsDB();
+        char* errMsg = 0;
+        
+        string sql = "DELETE FROM accounts WHERE account_id = " + to_string(accountID) + ";";
+        
+        int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg);
+        if (rc != SQLITE_OK) {
+            cout << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
             sqlite3_close(db);
-            return -1;
+            return 0;
         }
 
+        cout << "Account " << accountID << " has been successfully closed." << endl;
         sqlite3_close(db);
         return 1;
     }
-    
+
+    cout << "Account does not exist." << endl;
     return 0;
 }
