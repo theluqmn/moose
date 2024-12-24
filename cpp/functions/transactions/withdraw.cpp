9dac92cb70f577315af962f2e729f5f6cc54f89a
theluqmn <luqman.yazrie@outlook.com>
2024-12-24 22:34:35 +0800

    final C++. time to say goodbye, it has been a journey

--- cpp/functions/transactions/withdraw.cpp
+++ cpp/functions/transactions/withdraw.cpp
@@ -20,41 +20,41 @@ int fundsWithdraw(string accountType, int accountID, float amount) {
         return -1;
     }
 
-    // Check if account has sufficient balance
-    string sql = "SELECT balance FROM " + accountType + " WHERE account_id = " + to_string(accountID) + ";";
-    sqlite3_stmt *stmt;
-    int rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
+    // // Check if account has sufficient balance
+    // string sql = "SELECT balance FROM " + accountType + " WHERE account_id = " + to_string(accountID) + ";";
+    // sqlite3_stmt *stmt;
+    // int rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
 
-    // Error checking
-    if (rc != SQLITE_OK) {
-        cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << endl;
-        sqlite3_close(db);
-        return -1;
-    }
+    // // Error checking
+    // if (rc != SQLITE_OK) {
+    //     cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << endl;
+    //     sqlite3_close(db);
+    //     return -1;
+    // }
 
-    // Check if account has sufficient balance
-    rc = sqlite3_step(stmt);
-    if (rc == SQLITE_ROW) {
-        balance = sqlite3_column_double(stmt, 0);
-        if (balance < amount) {
-            cerr << "Insufficient balance" << endl;
-            sqlite3_finalize(stmt);
-            sqlite3_close(db);
-            return -1;
-        }
-        sqlite3_finalize(stmt);
-    }
+    // // Check if account has sufficient balance
+    // rc = sqlite3_step(stmt);
+    // if (rc == SQLITE_ROW) {
+    //     balance = sqlite3_column_double(stmt, 0);
+    //     if (balance < amount) {
+    //         cerr << "Insufficient balance" << endl;
+    //         sqlite3_finalize(stmt);
+    //         sqlite3_close(db);
+    //         return -1;
+    //     }
+    //     sqlite3_finalize(stmt);
+    // }
 
-    // Add amount to account
-    string sql = "UPDATE " + accountType + " SET balance = balance - " + to_string(amount) + " WHERE account_id = " + to_string(accountID) + ";";
-    char *errMsg = 0;
-    int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg);
+    // // Add amount to account
+    // string sql = "UPDATE " + accountType + " SET balance = balance - " + to_string(amount) + " WHERE account_id = " + to_string(accountID) + ";";
+    // char *errMsg = 0;
+    // int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg);
     
-    if (rc != SQLITE_OK) {
-        sqlite3_free(errMsg);
-        sqlite3_close(db);
-        return -1;
-    }
+    // if (rc != SQLITE_OK) {
+    //     sqlite3_free(errMsg);
+    //     sqlite3_close(db);
+    //     return -1;
+    // }
     
     sqlite3_close(db);
 
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,8 +2,6 @@
 #include <string>
 #include <map>
 
-#include <cpprest/http_listener.h>
-
 #include "prompts/prompts.h"
 #include "functions/functions.h"
 
@@ -14,6 +12,32 @@ int main() {
     cout << "MOOSE - simple bank management system" << endl;
     cout << "https://github.com/theluqmn/moose" << endl;
     cout << endl;
+    cout << "'quit' to exit | 'help' to open help" << endl;
+
+    string input;
+    
+    while (true) {
+        cout << "> ";
+        getline(cin, input);
+        
+        clearScreen();
+        if (input == "quit") {
+            cout << "till we meet again." << endl;
+            break;
+        } else if (input == "help") {
+            promptHelp();
+        } else if (input == "account open") {
+            promptOpenAccount();
+        } else if (input == "account close") {
+            promptCloseAccount();
+        } else if (input == "funds deposit") {
+            promptFundDeposit();
+        } else if (input == "funds transfer") {
+            promptTransfer();
+        } else {
+            cout << "Invalid input" << endl;
+        }
+    }
 
     return 0;
 }
