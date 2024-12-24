ab4c7c8a179f930719e632e111c31d1befe5cade
theluqmn <luqman.yazrie@outlook.com>
2024-12-24 19:40:52 +0800

    COMMANDS

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use prompted::input;
 
 fn main() {
-    let name: String = input!("Enter your name: ");
-    println!("Hello, {}!", name);
+    while let true = true {
+        let command: String = input!("> ");
+
+        if let "quit" = command.as_str() {
+            break;
+        } else if let "help" = command.as_str() {
+            println!("Commands: quit, help");
+        } else {
+            println!("Unknown command: {}", command);
+        }
+    }
 }
