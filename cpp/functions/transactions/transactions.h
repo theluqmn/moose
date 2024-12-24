3d15413dd2587151af26d5ce9ac5ebb22fbc9c89
theluqmn <luqman.yazrie@outlook.com>
2024-12-24 22:40:16 +0800

    Disposed of all the C++. goodbye

--- cpp/functions/transactions/transactions.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRANSACTIONS_H
-#define TRANSACTIONS_H
-
-#include <string>
-
-int fundsDeposit(std::string accountType, int accountID, float amount);
-int fundsWithdraw(std::string accountType, int accountID, float amount);
-
-#endif
