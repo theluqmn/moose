483a63c8c1b3f88839457cbade4b5314fc85d281
theluqmn <luqman.yazrie@outlook.com>
2024-12-24 19:33:57 +0800

    forgot gitignore for /target

--- target/debug/deps/moose-3ffcca8932cffb34.d
+++
@@ -1,5 +0,0 @@
-/home/theluqmn/Coding/moose/target/debug/deps/libmoose-3ffcca8932cffb34.rmeta: src/main.rs
-
-/home/theluqmn/Coding/moose/target/debug/deps/moose-3ffcca8932cffb34.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/moose-a979aebae9d42dfd.d
+++
@@ -1,5 +0,0 @@
-/home/theluqmn/Coding/moose/target/debug/deps/libmoose-a979aebae9d42dfd.rmeta: src/main.rs
-
-/home/theluqmn/Coding/moose/target/debug/deps/moose-a979aebae9d42dfd.d: src/main.rs
-
-src/main.rs:
