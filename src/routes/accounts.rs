bbd23b75e7b12786dc75418c80db8181eac85f5d
theluqmn <luqman.yazrie@outlook.com>
2024-12-25 04:24:49 +0800

    params

--- src/routes/accounts.rs
+++ src/routes/accounts.rs
@@ -1,6 +1,19 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct AccountCreate {
+    name: String,
+    password: String,
+}
 
 #[get("/accounts/create")]
-pub async fn accounts_create() -> impl Responder {
-    "Creating an account..."
+pub async fn accounts_create(query: web::Query<AccountCreate>) -> impl Responder {
+    let name = query.name.as_deref();
+    let password = query.password.as_deref();
+
+    println!("{}", name);
+    println!("{}", password);
+
+    HttpResponse::Ok().body("Account created!")
 }
