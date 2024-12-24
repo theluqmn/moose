3df7515bd5535b3763dcf5087e470f2a1db940ab
theluqmn <luqman.yazrie@outlook.com>
2024-12-25 03:29:33 +0800

    some progress on the database

--- src/functions/database.rs
+++ src/functions/database.rs
@@ -1,5 +1,30 @@
 use rusqlite::{Connection, Result};
 
-pub fn init_accounts() -> Result<()> {
+pub fn accounts_db() -> Result<()> {
+    struct Database {
+        conn: Connection,
+    };
+
+    impl Database {
+        // Intializes the database.
+        fn init() -> Result<Self> {
+            let conn = Connection::open("accounts.db")?;
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS checking (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    balance FLOAT DEFAULT 0.00,
+                    password TEXT,
+                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+                ",
+                [],
+            );
+
+            Ok(Self {conn})
+        }
+    }
+
     // Open the connection
 }
